653ebec863f442dde47f6b2392bf3f24f9dd1756
hoffsupes <dassgaurav93@gmail.com>
2021-10-27 04:11:41 -0400

    main-fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <trimat/video_matte_applier.h>
 #include <trimat/matte_applier.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) // main
 {
@@ -19,18 +20,18 @@ int main(int argc, char** argv) // main
   string bluesstillpath = string(argv[4]);
   string greenstillpath = string(argv[5]);
   string foregroundpath = string(argv[6]);
-  string alphapath = string(argv[7]);
-  string mattedvideopath = string(argv[8]);
-  double scale = stod(argv[9]);
-  bool video_only = bool(stoi(argv[10]));
-  bool display = bool(stoi(argv[11]));    // all parameters read
+  string alphapath = string(argv[7]); // all parameters read
 
-  if(mode.data() == "video-matte")
+  if(mode == string("video-matte"))
   {
+    string mattedvideopath = string(argv[8]);
+    double scale = atof(argv[9]);
+    bool video_only = bool(atoi(argv[10]));
+    bool display = bool(atoi(argv[11]));    // all parameters read
     VideoMatte vmatter = VideoMatte(bluepath,greenpath,bluesstillpath,greenstillpath); //
     vmatter.mattedVideoOutput(foregroundpath, alphapath,mattedvideopath, video_only,scale,display);   // matted video output
   }
-  elif(mode.data() == "image-matte")
+  else if(mode == string("image-matte"))
   {
     Image bluepath_image = Image(bluepath);
     Image greenpath_image = Image(greenpath);
